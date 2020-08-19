5234f2ba4c6777709313681e2edde7172a992eea
barrett4467 <barrett4467@gmail.com>
2020-08-18 20:59:44 -0600

    Add existing file, first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"; 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="container">
+          <nav>
+            <h2>MERN-Stack Todo App</h2>
+            <Link to="/">Todos</Link>
+            <Link to="/create">Create Todo</Link>
+          </nav>
+        </div>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </Router>
+    );
+  }
 }
 
 export default App;
